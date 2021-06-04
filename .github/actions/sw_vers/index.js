36892c82691ea0c24bffa02977c193d73b727626
Max Howell <mxcl@me.com>
2021-06-04 11:00:45 -0400

    Get Ubuntu & Xcode versions

--- .github/actions/sw_vers/index.js
+++ .github/actions/sw_vers/index.js
@@ -5,10 +5,30 @@ const gha_exec = require('@actions/exec')
 
 async function run() {
   try {
-    let stdout = await exec('sw_vers')
-    let matches = stdout.match(/^ProductVersion:\s+(.*)$/m)
-    if (!matches[1]) throw new Error("Regex match to `sw_vers` failed")
-    core.setOutput("macOS", matches[1])
+    core.info(process.platform)
+
+    switch (process.platform) {
+    case 'darwin':
+      let stdout = await exec('sw_vers')
+      let matches = stdout.match(/^ProductVersion:\s+(.*)$/m)
+      if (!matches[1]) throw new Error("Regex match to `sw_vers` failed")
+      core.setOutput("os", matches[1])
+      break
+    default:
+      const { readFileSync } = require('fs');
+
+      for (const line of readFileSync('/etc/os-release', {encoding: 'utf8'}).toString().split('\n')) {
+        const [key, value] = line.split('=');
+        if (key !== 'VERSION') continue;
+        const matches = value.match(/((\d+\.)*\d+)\s/);
+        if (matches && matches[1]) {
+          core.setOutput('os', matches[1]);
+          break;
+        }
+      }
+
+      break
+    }
 
     stdout = await exec('swift', ['--version'])
     matches = stdout.match(/Swift version (.+?)\s/m)
