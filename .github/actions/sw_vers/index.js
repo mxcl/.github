96d9fc6140fbabcc101b2a73278ffb1d1549851d
Max Howell <mxcl@me.com>
2021-06-09 10:19:18 -0400

    wip

--- .github/actions/sw_vers/index.js
+++ .github/actions/sw_vers/index.js
@@ -7,14 +7,21 @@ async function run() {
   try {
     core.info(process.platform)
 
+    let rootKey
+
     switch (process.platform) {
     case 'darwin':
       let stdout = await exec('sw_vers')
       let matches = stdout.match(/^ProductVersion:\s+(.*)$/m)
       if (!matches[1]) throw new Error("Regex match to `sw_vers` failed")
       core.setOutput("os", matches[1])
+
+      rootKey = `macOS-${matches[1]}`
+      core.setOutput("root-key", rootKey)
+
       break
     default:
+
       const { readFileSync } = require('fs');
 
       for (const line of readFileSync('/etc/os-release', {encoding: 'utf8'}).toString().split('\n')) {
@@ -23,6 +30,10 @@ async function run() {
         const matches = value.match(/((\d+\.)*\d+)\s/);
         if (matches && matches[1]) {
           core.setOutput('os', matches[1]);
+
+          rootKey = `macOS-${matches[1]}`
+          core.setOutput("root-key", rootKey)
+
           break;
         }
       }
